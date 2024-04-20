5153fc591e28202bac3ad8da3849d82234f765d5
tan205 <tanishqshivram@gmail.com>
2024-04-20 20:24:07 +0530

    cdf

---
+++ backend/user.js
@@ -0,0 +1,37 @@
+// models/User.js
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    mobileNo: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
